a06e39fab75d97f9d02e8fc2ee77caac5e9a805d
Viraj <virajthange45@gmail.com>
2026-06-08 12:22:02 +0530

    Added variables declaration with let

---
+++ Day_03/script.js
@@ -0,0 +1,14 @@
+let val = (43, 66, 23, 53); //Will assign the last value
+console.log(val); //53
+
+let x, y = 43;
+console.log(x); //undefined
+console.log(y); //43
+
+let a = (b = 4);
+console.log(a, b); // 4 4
+a = 5;
+console.log(a, b); //5  4
+
+let m = 10, n = 20;
+console.log(m, n); //10 20
